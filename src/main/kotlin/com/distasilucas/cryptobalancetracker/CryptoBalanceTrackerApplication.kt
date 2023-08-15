552033094cdb8c36de6f95078441052e20f82bac
Di Stasi Lucas <distasilucas@gmail.com>
2023-08-14 21:09:48 -0300

    CBT-1 - Initial commit

---
+++ src/main/kotlin/com/distasilucas/cryptobalancetracker/CryptoBalanceTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.distasilucas.cryptobalancetracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoBalanceTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptoBalanceTrackerApplication>(*args)
+}
