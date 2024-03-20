ce4795b922f7264f69aefbbfe5e74da56b1a06ac
Di Stasi Lucas <distasilucas@gmail.com>
2024-03-20 20:09:33 -0300

    CBT-25 - Price target feature

---
+++ src/main/kotlin/com/distasilucas/cryptobalancetracker/entity/PriceTarget.kt
@@ -0,0 +1,31 @@
+package com.distasilucas.cryptobalancetracker.entity
+
+import com.distasilucas.cryptobalancetracker.model.response.pricetarget.PriceTargetResponse
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.util.UUID
+
+@Document("PriceTarget")
+data class PriceTarget(
+  @Id
+  val id: String = UUID.randomUUID().toString(),
+
+  @Field("crypto_id")
+  val coingeckoCryptoId: String,
+  val target: BigDecimal
+) {
+
+  fun toPriceTargetResponse(cryptoName: String, currentPrice: BigDecimal, change: Float) =
+    PriceTargetResponse(id, cryptoName, currentPrice.toPlainString(), target.toPlainString(), change)
+
+  fun calculateChangeNeeded(currentPrice: BigDecimal): Float {
+    return target.subtract(currentPrice)
+      .divide(currentPrice, 3, RoundingMode.HALF_UP)
+      .multiply(BigDecimal("100"))
+      .setScale(2, RoundingMode.HALF_UP)
+      .toFloat()
+  }
+}
