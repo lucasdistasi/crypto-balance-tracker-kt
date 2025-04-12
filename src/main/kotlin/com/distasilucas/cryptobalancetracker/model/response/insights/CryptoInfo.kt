d37724497b5e99f7ea7c8fa6fb77a2208c6623fc
Di Stasi Lucas <distasilucas@gmail.com>
2025-04-12 17:53:47 -0300

    CBT-32 - Make cache data classes serializable

--- src/main/kotlin/com/distasilucas/cryptobalancetracker/model/response/insights/CryptoInfo.kt
+++ src/main/kotlin/com/distasilucas/cryptobalancetracker/model/response/insights/CryptoInfo.kt
@@ -2,6 +2,7 @@ package com.distasilucas.cryptobalancetracker.model.response.insights
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.io.Serializable
 import java.math.BigDecimal
 
 // TODO - instead of using nullable fields, maybe create new class for PlatformInsightsResponse
@@ -16,13 +17,13 @@ data class CryptoInfo(
   val image: String,
   val currentPrice: CurrentPrice? = null,
   val priceChange: PriceChange? = null,
-)
+): Serializable
 
 data class CurrentPrice(
   val usd: String,
   val eur: String,
   val btc: String
-) {
+): Serializable {
   constructor(usd: BigDecimal, eur: BigDecimal, btc: BigDecimal) : this(
     usd.toPlainString(), eur.toPlainString(), btc.toPlainString()
   )
@@ -32,4 +33,4 @@ data class PriceChange(
   val changePercentageIn24h: BigDecimal,
   val changePercentageIn7d: BigDecimal,
   val changePercentageIn30d: BigDecimal
-)
+): Serializable
--- src/main/kotlin/com/distasilucas/cryptobalancetracker/model/response/insights/UserCryptoInsights.kt
+++ src/main/kotlin/com/distasilucas/cryptobalancetracker/model/response/insights/UserCryptoInsights.kt
@@ -1,8 +1,10 @@
 package com.distasilucas.cryptobalancetracker.model.response.insights
 
+import java.io.Serializable
+
 data class UserCryptoInsights(
   val cryptoInfo: CryptoInfo,
   val quantity: String,
   val percentage: Float,
   val balances: BalancesResponse,
-)
+): Serializable
